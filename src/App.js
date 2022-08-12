39ee5aec1d1ac7510813c8315da436fcf7e4d2cb
Matheus Ramos <msilvaramos84@gmail.com>
2022-08-12 14:56:16 -0300

    Providing theme for app and removing strict mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import MainPage from './pages/MainPage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PrivateRoutes from './routes/PrivateRoutes';
+import React from "react";
+import MainPage from "./pages/MainPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./routes/PrivateRoutes";
+import mainTheme from "./themes/main";
+import { ThemeProvider } from "@emotion/react";
 
 const App = () => {
     return (
-        <div className="app">                    
-            <BrowserRouter>
-                <Routes>
-                    <Route exact element={<PrivateRoutes/>}>
-                        <Route path='/' exact element={<MainPage/>}/>
-                    </Route>
-                    <Route path='/login' exact element={<Login />}/>
-                    <Route path='/signup' exact element={<Signup />}/>
-                </Routes>
-            </BrowserRouter>
+        <div className="app">
+            <ThemeProvider theme={mainTheme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route exact element={<PrivateRoutes />}>
+                            <Route path="/" exact element={<MainPage />} />
+                        </Route>
+                        <Route path="/login" exact element={<Login />} />
+                        <Route path="/signup" exact element={<Signup />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
         </div>
     );
-}
+};
 
 export default App;
