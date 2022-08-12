a302757509ed8c77ef5a7da48a3c2a017d6abd35
Matheus Ramos <msilvaramos84@gmail.com>
2022-08-12 14:57:11 -0300

    Modifiyng themes

---
+++ src/themes/main.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#323635"
+        },
+        secondary: {
+            main: "#f2eeed"
+        }
+    },
+});
+
+export default theme;
