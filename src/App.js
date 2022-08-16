49bc6e81cd9441ace9388d2e547ad95091314581
Matheus Ramos <msilvaramos84@gmail.com>
2022-08-16 15:57:56 -0300

    Redirecting to homepage in case of url mismatch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MainPage from "./pages/MainPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoutes from "./routes/PrivateRoutes";
 import mainTheme from "./themes/main";
 import { ThemeProvider } from "@emotion/react";
@@ -15,6 +15,8 @@ const App = () => {
                     <Routes>
                         <Route exact element={<PrivateRoutes />}>
                             <Route path="/" exact element={<MainPage />} />
+                            <Route path="/*" exact element={<Navigate to="/" />}
+                            />
                         </Route>
                         <Route path="/login" exact element={<Login />} />
                         <Route path="/signup" exact element={<Signup />} />
